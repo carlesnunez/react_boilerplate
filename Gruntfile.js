f1d6cf2c410254f8e64bdd44c8b9f9089352077a
Xavi Serra <xavier.serra@zitrointeractive.com>
2015-07-20 11:49:47 +0200

    (FEAT) Cleaning gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,40 +8,24 @@ module.exports = function(grunt) {
         browserify: {
             build: {
                 src: ['src/**/*.js'],
-                dest: '.build/main.js',
+                dest: '.build/index.js',
                 options: {
                     transform: ['babelify'],
                     require: ['babelify/polyfill']
                 }
             }
         },
-
-        uglify: {
-            build: {
-                src: 'dist/seed.js',
-                dest: 'dist/seed.min.js'
-            }
-        },
-
         copy: {
             assets: {
                 files: [
-                    {src: './assets/*', expand: true, cwd: 'src', dest: '.build/'}
+                    {cwd: 'src', expand: true, src: './assets/*', dest: '.build/'}
                 ]
             }
         },
-
-        jsdoc: {
-            all: {
-                src: ['src/**/*.js', 'tests/**/*.js'],
-                dest: 'doc'
-            }
-        },
-
         watch: {
             all: {
                 files: ['src/**/*.js', 'tests/**/*.js'],
-                tasks: ['jshint:all'],
+                tasks: ['default'],
                 options: {
                     spawn: false
                 }
@@ -63,38 +47,6 @@ module.exports = function(grunt) {
                 autoWatch: true,
                 singleRun: false
             }
-        },
-
-        jshint: {
-            options: {
-                bitwise: true,
-                camelcase: true,
-                curly: true,
-                eqeqeq: true,
-                forin: true,
-                freeze: true,
-                indent: 4,
-                latedef: true,
-                newcap: true,
-                noarg: true,
-                noempty: true,
-                nonbsp: true,
-                nonew: true,
-                plusplus: true,
-                quotmark: 'single',
-                undef: true,
-                unused: true,
-                strict: true,
-                maxlen: 120,
-                browser: true,
-                devel: false,
-                jasmine: true,
-                browserify: true,
-                esnext: true
-            },
-            all: {
-                src: ['Gruntfile.js', 'src/**/*.js', 'tests/**/*.js']
-            }
         }
     };
 
@@ -103,18 +55,14 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-force-task');
-    grunt.loadNpmTasks('grunt-jsdoc');
     grunt.loadNpmTasks('grunt-karma');
 
     grunt.event.on('watch', function(action, path) {
         grunt.config('jshint.all.src', path);
     });
 
-    grunt.registerTask('build', ['clean:all', 'jshint:all', 'browserify:build', 'copy:assets']);
+    grunt.registerTask('build', ['clean:all', 'browserify:build', 'copy:assets']);
 
     grunt.registerTask('default', ['build', 'watch:all']);
 
